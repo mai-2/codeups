c7e3fd69ed9872a33ca77f563d9cfbf38c04a794
mai-2 <71576918+mai-2@users.noreply.github.com>
2022-06-02 01:30:51 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,15 +3,14 @@ const swiper = new Swiper('.swiper', {
   centeredSlides: true,
   slidesPerView: 1,
   spaceBetween: 15,
-  
+  autoplay: {
+       delay: 3000,
+      },
 
   breakpoints: {
     767: {
       slidesPerView: 4.1,
       spaceBetween: 40,
-      autoplay: {
-       delay: 3000,
-      },
     },
   },
 });
